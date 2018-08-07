cd307a34db28b629c0d51c5bd78b303e2c6f65b0
Ludovic Ceccotti <ludovic.ceccotti@gmail.com>
2018-08-07 21:10:45 +0200

    notifications

--- app/Http/Controllers/changeInfosController.php
+++ app/Http/Controllers/changeInfosController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
+use Illuminate\Support\Facades\Hash;
+use App\Notifications\updateInfos;
+use Illuminate\Support\Facades\Notification;
 
 class changeInfosController extends Controller
 {
-    public function create(){
+    public function create(Request $request){
+      if($request->session()->has('email')){
+        echo $request->session()->get('email');
+      }else{echo "pok";}
       return view('change-infos');
     }
     public function store(Request $request){
@@ -25,5 +31,32 @@ class changeInfosController extends Controller
       $new_email=filter_var($datas['new_email'],FILTER_SANITIZE_EMAIL);
       $current_password=filter_var($datas['current_password'],FILTER_SANITIZE_STRING);
       $new_password=filter_var($datas['new_password'],FILTER_SANITIZE_STRING);
+
+      $pseudo=DB::table('users')->where('email',$request->session()->get('email'))->value('pseudo');
+      $email=DB::table('users')->where('email',$request->session()->get('email'))->value('email');
+      $password=DB::table('users')->where('email',$request->session()->get('email'))->value('password');
+
+
+      if($current_pseudo==$pseudo){
+        DB::table('users')->where('email',$request->session()->get('email'))->update(['pseudo'=>$new_pseudo]);
+        echo "pseudo updated";
+        Notification::route('mail', 'f4c41f6920-92df21@inbox.mailtrap.io')->notify(new updateInfos("The BeCode staff"));
+
+      }else{echo "The pseudo you have typed doesn't match your current registered pseudo";}
+      if ($current_email==$email) {
+          DB::table('users')->where('email',$request->session()->get('email'))->update(['email'=>$new_email]);
+          Notification::route('mail', 'f4c41f6920-92df21@inbox.mailtrap.io')->notify(new updateInfos("The BeCode staff"));
+          echo 'email adress updated';
+      }else {
+        echo "The email adress you have typed doesn't match your current registered email adress";
+      }
+      if(Hash::check($current_password,$password)==true){
+          $new_password=Hash::make($new_password);
+          DB::table('users')->where('email',$request->session()->get('email'))->update(['password'=>$new_password]);
+          Notification::route('mail', 'f4c41f6920-92df21@inbox.mailtrap.io')->notify(new updateInfos("The BeCode staff"));
+          echo "password updated succesfully";
+      }else {
+        echo "The password you have typed doesn't match your current registered password";
+      }
     }
 }
