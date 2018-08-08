681451cd8312391c605690d7de50b3d5a4432862
Daniel Muyshond <daniel.muyshond@gmail.com>
2018-08-08 13:17:09 +0200

    afterlunch commit (the most unsignificant commit description)

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,33 +5,44 @@
     <title>Login</title>
   </head>
   <body>
-    <form class="" action="#" method="post">
-      @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-              @foreach ($errors->all() as $error)
-              <li>{{ $error }}</li>
-              @endforeach
-            </ul>
-          </div>
-        @endif
-    <input type = "hidden" name = "_token" value = "<?php echo csrf_token() ?>" />
-      <p><label for="pseudo">Pseudo</label></p>
-      <p><input type="text" name="login_pseudo" value=""/></p>
-      <p><label for="email">Email</label></p>
-      <p><input type="email" name="login_email"/></p>
-      <p><label for="password">Password</label></p>
-      <p><input type="password" name="login_password"/></p>
-      <p><label for="status">Vous êtes:</label></p>
-      <p><select name="login_satus">
-        <option value="Apprenant">Apprenant</option>
-        <option value="Coach">Coach</option></select></p>
-      <p><label for="key">Clé d'identification</label></p>
-      <p><input type="password" name="login_key"/></p>
-      <!-- <p><label for="remember">Remember me</label></p>
-      <p><input type="checkbox" value="remember me" name="remember"/></p> -->
-      <p><input type="submit" name="login_submit" value="login"/></p>
-      <p><a href=<?php echo url('/register'); ?>>Not registered? Create an account</a></p>
-    </form>
+    <main>
+
+      <header>
+        <img src="/assets/img/app_image192.png" alt="Becode CodeChain app image" title="Becode CodeChain app image">
+        {{-- <img src="{{URL::asset('/image/propic.png')}}" alt="profile Pic" height="200" width="200"> --}}
+      </header>
+
+      <section>
+      <form class="" action="#" method="post">
+        @if ($errors->any())
+          <div class="alert alert-danger">
+              <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+              </ul>
+            </div>
+          @endif
+      <input type = "hidden" name = "_token" value = "<?php echo csrf_token() ?>" />
+        <p><label for="pseudo">Pseudo</label></p>
+        <p><input type="text" name="login_pseudo" value=""/></p>
+        <p><label for="email">Email</label></p>
+        <p><input type="email" name="login_email"/></p>
+        <p><label for="password">Password</label></p>
+        <p><input type="password" name="login_password"/></p>
+        <p><label for="status">Vous êtes:</label></p>
+        <p><select name="login_satus">
+          <option value="Apprenant">Apprenant</option>
+          <option value="Coach">Coach</option></select></p>
+        <p><label for="key">Clé d'identification</label></p>
+        <p><input type="password" name="login_key"/></p>
+        <!-- <p><label for="remember">Remember me</label></p>
+        <p><input type="checkbox" value="remember me" name="remember"/></p> -->
+        <p><input type="submit" name="login_submit" value="login"/></p>
+        <p><a href=<?php echo url('/register'); ?>>Not registered? Create an account</a></p>
+      </form>
+      </section>
+
+    </main>
   </body>
 </html>
